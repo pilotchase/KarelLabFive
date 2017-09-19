701870800a588280a6a9dfa5fafb3bb43c96d8a6
Chase Morgan <pilotchase@users.noreply.github.com>
2017-09-19 12:20:07 -0700

    Problem1 Complete

--- DangerousBot.java
+++ DangerousBot.java
@@ -12,7 +12,48 @@ public class DangerousBot extends Robot
     }
     
     public void choosePile() {
-        
+        // false return = odd, true return = even
+        if(oddOrEven()){
+            pickEast();
+        } else {
+            pickWest();
+        }
+    }
+    
+    public boolean oddOrEven(){
+        int count = 0;
+        while(nextToABeeper()){
+            pickBeeper();
+            count++;
+        }
+        if(count % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+    
+    public void pickEast(){
+        turnLeft();
+        turnLeft();
+        turnLeft();
+        move();
+        while(nextToABeeper()){
+            pickBeeper();
+        }
+        turnLeft();
+        turnLeft();
+        move();
+    }
+    
+    public void pickWest(){
+        turnLeft();
+        move();
+        while(nextToABeeper()){
+            pickBeeper();
+        }
+        turnLeft();
+        turnLeft();
+        move();
     }
    
 }
